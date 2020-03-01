443c0c4a508368141c300d2160c29ab813b3eef1
Aning <413149574@qq.com>
2020-03-01 15:43:03 +0800

    singleton  class

---
+++ src/main/java/com/fei/design/pattern/prototype/json/JsonPrototype.java
@@ -0,0 +1,54 @@
+package com.fei.design.pattern.prototype.json;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.List;
+
+public class JsonPrototype {
+    private String name;
+
+    private Integer id;
+
+
+    private List<String> types;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public List<String> getTypes() {
+        return types;
+    }
+
+    public void setTypes(List<String> types) {
+        this.types = types;
+    }
+
+    @Override
+    protected JsonPrototype clone() throws CloneNotSupportedException {
+        String jsonStr= JSONObject.toJSONString(this);
+        JSONObject jsonObject = JSON.parseObject(jsonStr);
+        return JSONObject.toJavaObject(jsonObject,JsonPrototype.class);
+    }
+
+    @Override
+    public String toString() {
+        return "JsonPrototype{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", types=" + types .toString()+
+                '}';
+    }
+}
